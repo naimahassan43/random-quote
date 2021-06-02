2a7fc1db249a4a3f0d9b073c419757ca0bcec805
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-06-02 23:02:45 +0100

    Initialise project

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/******************************************
+project 1 - Random Quote Generator
+******************************************/
+
+/***
+ * `quotes` array
+ ***/
+const quotes = [{
+    quote: '',
+    source: '',
+    citation: '',
+    year: ''
+}];
+/***
+ * `getRandomQuote` function
+ ***/
+
+/***
+ * `printQuote` function
+ ***/
+
+/***
+ * click event listener for the print quote button
+ ***/
