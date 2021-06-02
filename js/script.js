2486312941d420bed4399e2b0db3687095824d63
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-06-03 00:53:46 +0100

    Create array of objects

--- js/script.js
+++ js/script.js
@@ -6,13 +6,74 @@ project 1 - Random Quote Generator
  * `quotes` array
  ***/
 const quotes = [{
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
-}];
-/***
- * `getRandomQuote` function
+        quote: 'Whoever is happy will make others happy too.',
+        source: 'Anne Frank',
+        citation: '',
+        year: '2017'
+    },
+    {
+        quote: 'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
+        source: 'Benjamin Franklin',
+        citation: '',
+        year: '2015'
+    },
+
+    {
+        quote: 'When you reach the end of your rope, tie a knot in it and hang on.',
+        source: 'Franklin D. Roosevelt',
+        citation: '',
+        year: '2014'
+    },
+    {
+        quote: 'Always remember that you are absolutely unique. Just like everyone else.',
+        source: 'Margaret Mead',
+        citation: '',
+        year: '2012'
+    },
+    {
+        quote: 'Do not go where the path may lead, go instead where there is no path and leave a trail. ',
+        source: 'Ralph Waldo Emerson',
+        citation: '',
+        year: '2015'
+    },
+    {
+        quote: 'The future belongs to those who believe in the beauty of their dreams.',
+        source: 'Eleanor Roosevelt',
+        citation: '',
+        year: '2016'
+    },
+    {
+        quote: 'Donot judge each day by the harvest you reap but by the seeds that you plant.',
+        source: 'Robert Louis Stevenson',
+        citation: '',
+        year: '2012'
+    },
+    {
+        quote: 'Always remember that you are absolutely unique. Just like everyone else.',
+        source: 'Margaret Mead',
+        citation: '',
+        year: '2015'
+    },
+    {
+        quote: 'When you reach the end of your rope, tie a knot in it and hang on.',
+        source: 'Franklin D. Roosevelt',
+        citation: '',
+        year: '2011'
+    },
+    {
+        quote: 'The future belongs to those who believe in the beauty of their dreams. ',
+        source: 'Eleanor Roosevelt',
+        citation: '',
+        year: '2012'
+    },
+    {
+        quote: 'Donot judge each day by the harvest you reap but by the seeds that you plant.',
+        source: 'Robert Louis Stevenson',
+        citation: '',
+        year: '2015'
+    },
+];
+/**** `getRandomQuote` function
  ***/
 
 /***
